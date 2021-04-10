2ad3ed4a13e7f1d8bf74715357d83f7601f542e1
Charlie <jclopex@gmail.com>
2021-04-10 14:50:09 -0500

    typo in app.py & migrations executed

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ from src.models import db
 app = Flask(__name__)
 
 # configuramos la DB en la APP
-app.config['SQLALCHEMY_DATBASE_URI'] = "postgresql://admin:123456@localhost:5432/bootcamp"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://admin:123456@localhost:5432/bootcamp"
 
-# PAra que la BD maneja toda la info de la APP
+# Para que la BD maneja toda la info de la APP
+from src.models import *
 db.init_app(app)
 db.app = app
-db.create_all()
+#db.create_all()
 
 @app.route("/")
 def hola_mundo():
